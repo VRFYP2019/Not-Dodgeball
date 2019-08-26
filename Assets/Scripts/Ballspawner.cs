331d1e9c4b66a37454d4527209780b3fd437f6ea
khooroko <khoowp25@hotmail.com>
2019-08-26 23:15:50 +0800

    Import SteamVR and get some smacking done

---
+++ Assets/Scripts/Ballspawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballspawner : MonoBehaviour
+{
+    public GameObject obj;
+    float timeLastSpawned = 0;
+    public float spawnInterval = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLastSpawned = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeLastSpawned > spawnInterval) {
+            Spawn();
+            timeLastSpawned = Time.time;
+        }
+    }
+
+    public void Spawn() {
+        Instantiate(obj);
+    }
+}
