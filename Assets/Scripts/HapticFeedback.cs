b58d552017855a14fee69928ab2142f16e825dc3
khooroko <khoowp25@hotmail.com>
2019-08-27 09:19:55 +0800

    Add haptic feedback

---
+++ Assets/Scripts/HapticFeedback.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HapticFeedback : MonoBehaviour
+{
+    public SteamVR_Action_Vibration vibration;
+    SteamVR_Input_Sources trackedObj;
+    // Start is called before the first frame update
+    private void Start() {
+        trackedObj = GetComponent<SteamVR_Behaviour_Pose>().inputSource;
+    }
+
+    public void Vibrate(float duration, float frequency, float amplitude) {
+        vibration.Execute(0, duration, frequency, amplitude, trackedObj);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        vibration.Execute(0, 1, 150, 75, trackedObj);
+        Debug.Log("collide");
+    }
+}
---
+++ Assets/Scripts/Tool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    HapticFeedback hapticFeedback;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hapticFeedback = GetComponentInParent<HapticFeedback>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        hapticFeedback.Vibrate(0.1f, 100, 15);
+    }
+}
