e50be9381e9471d320cb5fcfb5f506f2a92df15c
khooroko <khoowp25@hotmail.com>
2019-09-02 17:42:40 +0800

    Add switching hands

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -5,10 +5,15 @@ using Valve.VR;
 
 public class Person : MonoBehaviour {
     public bool isRightHandSpawner;
+    public SteamVR_Action_Boolean grab = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
     HandController leftHandController;
     HandController rightHandController;
+    SpawnerHand currentSpawner = null;
     SteamVR_Behaviour_Pose leftHand;
     SteamVR_Behaviour_Pose rightHand;
+    [SerializeField]
+    private bool _isSpawning;
+    public bool IsSpawning { get { return _isSpawning; } set { _isSpawning = value; } }
 
     // Start is called before the first frame update
     void Start() {
@@ -23,12 +28,30 @@ public class Person : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (BallManager.Instance.PlayerBallQueues[0].Count > 0
-            && !rightHandController.isSpawning && !leftHandController.isSpawning ) {
-            if (isRightHandSpawner) {
-                rightHandController.SwitchToSpawnerHand();
-            } else {
-                leftHandController.SwitchToSpawnerHand();
+        if (BallManager.Instance.PlayerBallQueues[0].Count > 0) {
+            if (!_isSpawning) {
+                _isSpawning = true;
+                if (isRightHandSpawner) {
+                    rightHandController.SwitchToSpawnerHand(null);
+                    currentSpawner = rightHand.GetComponentInChildren<SpawnerHand>();
+                } else {
+                    leftHandController.SwitchToSpawnerHand(null);
+                    currentSpawner = leftHand.GetComponentInChildren<SpawnerHand>();
+                }
+            }
+        }
+        if (grab.GetStateDown(SteamVR_Input_Sources.Any)) {
+            isRightHandSpawner = !isRightHandSpawner;
+            if (_isSpawning) {
+                if (isRightHandSpawner) {
+                    rightHandController.SwitchToSpawnerHand(currentSpawner.currentBall);
+                    currentSpawner = rightHand.GetComponentInChildren<SpawnerHand>();
+                    leftHandController.SwitchToTool();
+                } else {
+                    leftHandController.SwitchToSpawnerHand(currentSpawner.currentBall);
+                    currentSpawner = leftHand.GetComponentInChildren<SpawnerHand>();
+                    rightHandController.SwitchToTool();
+                }
             }
         }
     }
