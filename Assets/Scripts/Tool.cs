5fa7e8e7098cb9660ae40a50de3e05de0a080b04
khooroko <khoowp25@hotmail.com>
2019-09-05 20:44:02 +0800

    Fix clipping through tool at high speeds
    By turning isTrigger on, so that the ball does not bounce off the other
    side of the tool

--- Assets/Scripts/Tool.cs
+++ Assets/Scripts/Tool.cs
@@ -6,17 +6,37 @@ using Valve.VR;
 public class Tool : MonoBehaviour {
     HapticFeedback hapticFeedback;
     SteamVR_Behaviour_Pose hand;
+    Collider thisCollider;  // calling it "collider" triggers a useless warning so it'll be called "thisCollider" instead
     public float forceMultiplier = 5;
+    readonly float velocityUpperThreshold = 1;  // anything above this value will be considered "fast" and treated differently to avoid clipping
 
     // Start is called before the first frame update
     void Start() {
         hapticFeedback = GetComponentInParent<HapticFeedback>();
         hand = GetComponentInParent<SteamVR_Behaviour_Pose>();
+        thisCollider = GetComponent<Collider>();
     }
 
+    private void Update() {
+        float handVelocityMagnitude = hand.GetVelocity().magnitude;
+        if (handVelocityMagnitude < velocityUpperThreshold) {
+            thisCollider.isTrigger = false;
+        } else {
+            thisCollider.isTrigger = true;
+        }
+    }
+
+    // Used for slow movement
     private void OnCollisionEnter(Collision collision) {
         hapticFeedback.Vibrate(0.1f, 100, 15);
         ExertForce(collision.collider.GetComponent<Rigidbody>());
+    }
+
+    // Used for fast movement
+    private void OnTriggerEnter(Collider other) {
+        hapticFeedback.Vibrate(0.1f, 100, 30);
+        // TODO: account for bounce using normal of supposed collision point
+        ExertForce(other.GetComponent<Rigidbody>());
     }
 
     // Exert a force on the other rigidbody
