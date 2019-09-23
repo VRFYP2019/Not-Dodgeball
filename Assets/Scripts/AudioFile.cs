5de65e7c18c1ebb8ddbe52cb3dbf3cf9c0a20a24
lyhvictoria <limyhvictoria31@gmail.com>
2019-09-23 18:59:24 +0800

    Add AudioManager for 2D sounds

---
+++ Assets/Scripts/AudioFile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioFile {
+    public string audioName;
+    public AudioClip audioClip;
+
+    [Range(0f,1f)]
+    public float volume;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public bool isLooping;
+    public bool playOnAwake;
+
+}
