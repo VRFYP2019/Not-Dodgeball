cef4f1b3a03fc4fe54de9ad891450b0ecb3eb2a0
Khoo Wei Ping <khoowp25@hotmail.com>
2019-09-23 11:45:26 +0800

    Update WallIndicator.cs
    Remove empty function

--- Assets/Scripts/WallIndicator.cs
+++ Assets/Scripts/WallIndicator.cs
@@ -13,11 +13,6 @@ public class WallIndicator : MonoBehaviour {
 
     private float distFromLWall, distFromRWall, distFromBWall;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
     // Update is called once per frame
     void Update() {
         distFromLWall = playerLocation.position.x - X_MIN;
