8413c33b8e665413497b1b3e75c2e6fc031bd60e
Lim Yan Hui <e0052689@nus.edu.sg>
2019-09-25 16:24:02 +0800

    Added Photon

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,45 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+	public GameObject joinGameButton;
+	private string ROOM_NAME = "TestRoomName";
+
+	// Start is called before the first frame update
+	void Start() {
+		Debug.Log("Connecting to Photon . . .");
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public override void OnConnectedToMaster() {
+		Debug.Log("Connected! To server: " + PhotonNetwork.CloudRegion);
+		PhotonNetwork.AutomaticallySyncScene = true;
+
+		RoomOptions roomOptions = new RoomOptions() { };
+		PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message) {
+		Debug.Log("Fak Create Failed: " + message);
+	}
+
+	public override void OnJoinedLobby() {
+		Debug.Log("Joined Lobby");
+
+		//RoomOptions roomOptions = new RoomOptions() { };
+		//PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
+	}
+
+	public override void OnJoinedRoom() {
+		Debug.Log("Joined Room :)");
+		//PhotonNetwork.Instantiate("", Vector3.zero, Quaternion.identity, 0);
+	}
+
+	void OnJoinedRoomFailed(short returnCode, string message) {
+		Debug.Log("Fak Joined Failed: " + message);
+	}
+}
