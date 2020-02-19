e8241e9f7f4a37ba7002a16bdfcfc83676812634
khooroko <khoowp25@hotmail.com>
2020-02-19 23:31:34 +0800

    Integrate Movesense

---
+++ Assets/Scripts/LobbyMovesensePanel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyMovesensePanel : MonoBehaviour {
+    [SerializeField]
+    private GameObject connectMovesenseButton = null;
+    [SerializeField]
+    private GameObject movesenseConnectedTextPanel = null;
+    [SerializeField]
+    private Text heartRateText = null;
+
+    // Start is called before the first frame update
+    void Start() {
+        // No Oculus no Movesense
+        if (OVRPlugin.productName == null || !OVRPlugin.productName.StartsWith("Oculus")) {
+            return;
+        }
+
+        if (FindObjectOfType<MovesenseController>() == null
+            || !MovesenseDevice.IsAnyConnectedOrConnecting()) {
+            OnMovesenseDisconnected();
+        } else {
+            OnMovesenseConnected();
+        }
+    }
+
+    private void Update() {
+        if (MovesenseSubscriber.instance == null) { // do nothing in update if not connected
+            return;
+        }
+        if (!MovesenseSubscriber.instance.isConnected) {
+            OnMovesenseDisconnected();
+        }
+        if (MovesenseSubscriber.instance.heartRate != null) {
+            heartRateText.text = MovesenseSubscriber.instance.heartRate.ToString();
+        } else {    // if heartrate is null
+            heartRateText.text = ("--");
+        }
+    }
+
+    private void OnMovesenseConnected() {
+        connectMovesenseButton.SetActive(false);
+        movesenseConnectedTextPanel.SetActive(true);
+    }
+
+    private void OnMovesenseDisconnected() {
+        heartRateText.text = ("--");
+        connectMovesenseButton.SetActive(true);
+        movesenseConnectedTextPanel.SetActive(false);
+    }
+
+    public void GoToScanScene() {
+        SceneManager.LoadScene("ScanScene");
+    }
+}
---
+++ Assets/Scripts/MovesenseSubscriber.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovesenseSubscriber : MonoBehaviour {
+    public static MovesenseSubscriber instance;
+    public string MacID;
+    public string Serial;
+    public int? heartRate = null;
+    public bool isConnected = false;
+
+    private void Awake() {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        MovesenseController.Event += OnMovesenseControllerCallbackEvent;
+        DontDestroyOnLoad(this);
+    }
+
+    void OnMovesenseControllerCallbackEvent(object sender, MovesenseController.EventArgs e) {
+        switch (e.Type) {
+            case MovesenseController.EventType.CONNECTED:       // a sensor succesfully connected
+                isConnected = true;
+                break;
+            case MovesenseController.EventType.DISCONNECTED:    // a sensor disconnected
+                isConnected = false;
+                heartRate = null;
+                break;
+            case MovesenseController.EventType.NOTIFICATION:    // got data from a sensor
+                for (int i = 0; i < e.OriginalEventArgs.Count; i++) {
+                    var ne = (NotificationCallback.EventArgs)e.OriginalEventArgs[i];
+                    RefreshHeartrate(ne);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void RefreshHeartrate(NotificationCallback.EventArgs e) {
+        if (e == null) {
+            return;
+        }
+
+        if (e.Subscriptionpath == SubscriptionPath.HeartRate) {
+            var notificationHeartRateArgs = (NotificationCallback.HeartRateArgs)e;
+            heartRate = (int)notificationHeartRateArgs.Pulse;
+        }
+    }
+}
