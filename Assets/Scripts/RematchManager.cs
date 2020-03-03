1326204027ff213388a5a8c00672cc6c357f51dd
khooroko <khoowp25@hotmail.com>
2020-03-04 00:42:32 +0800

    Improve exit handling from rematch screen

--- Assets/Scripts/RematchManager.cs
+++ Assets/Scripts/RematchManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RematchManager : MonoBehaviour {
@@ -19,14 +20,18 @@ public class RematchManager : MonoBehaviour {
         if (NetworkController.Instance != null) {
             NetworkController.Instance.PlayerLeaveRoom();
         } else {    // Probably lobby not loaded/built
+            #if UNITY_EDITOR
+            EditorApplication.Exit(0);
+            #else
             Application.Quit();
+            #endif       
         }
     }
 
     public void Restart() {
-        #if !UNITY_EDITOR
+#if !UNITY_EDITOR
         OculusUIHandler.instance.laserLineRenderer.enabled = false;
-        #endif
+#endif
         rematchCanvas.gameObject.SetActive(false);
         GameManager.Instance.Restart();
         if (NetworkController.Instance != null) {
