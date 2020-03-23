6c00c585bd471b2bbd25b75305df9d7db9a56a68
lyhvictoria <limyhvictoria31@gmail.com>
2020-03-23 16:24:25 +0800

    Add hats to OVRplayer

---
+++ Assets/Scripts/PlayerHat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerHat : MonoBehaviourPunCallbacks {
+    private Vector3 parentPos;
+
+    void Start() {
+        if (PhotonNetwork.IsConnected && !photonView.IsMine) {
+            enabled = false;
+        }
+    }
+
+    void Update() {
+        UpdateHatPosition();
+    }
+
+    private void UpdateHatPosition() {
+        parentPos = transform.parent.position;
+        transform.position = parentPos;
+    }
+}
